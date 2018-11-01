953b2d27b22f82e8cf26862da3d860a5036c0818
Graham Hewett <hewettgraham@gmail.com>
2018-11-01 13:19:05 +0000

    add 3x2 grid of tv show cover images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import blackMirrorImg from './blackMirror.jpg';
-import breakingBadImg from './breakingBad.jpg'
-import deathNoteImg from './deathNote.jpg'
-import gameOfThronesImg from './gameofThrones.jpg'
-import WalkingDeadImg from './walkingDead.jpg'
-import WireImg from './theWire.jpg'
+import breakingBadImg from './breakingBad.jpg';
+import deathNoteImg from './deathNote.jpg';
+import GOTImg from './gameofThrones.jpg';
+import theWireImg from './theWire.jpg';
+import walkingDeadImg from './walkingDead.jpg';
 
 
 class App extends Component {
@@ -13,19 +13,20 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <div className = "tvShows">
-          <h1 className="tv">Black Mirror</h1>
-          <h1 className="tv">Breaking Bad</h1>
-          <h1 className="tv">Death Note</h1>
-          <h1 className="tv">Game of Thrones</h1>
-          <h1 className="tv">The Walking Dead</h1>
-          <h1 className="tv">The Wire</h1>
-          </div>
         </header>
-        <body>
-
-        </body>
-      </div>
+          <main>
+          <div className = "row">
+            <div className="column"><img src = {blackMirrorImg} alt ="A"></img></div>
+            <div className="column"><img src = {breakingBadImg} alt ="A"></img></div>
+            <div className="column"><img src = {deathNoteImg} alt ="A"></img></div>
+          </div>
+          <div className = "row">
+            <div className="column"><img src = {GOTImg} alt ="A"></img></div>
+            <div className="column"><img src = {theWireImg} alt ="A"></img></div>
+            <div className="column"><img src = {walkingDeadImg} alt ="A"></img></div>
+          </div>
+          </main>
+        </div>
     );
   }
 }
