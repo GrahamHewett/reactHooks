a86b2211df0af6bb11f4c254c444ae93f4fbc370
Graham Hewett <hewettgraham@gmail.com>
2018-11-01 12:03:22 +0000

    Create 6 tv show titles. Edit css to align titles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
-import movieCover from './pickOfDestiny.jpg';
 import './App.css';
+import blackMirrorImg from './blackMirror.jpg';
+import breakingBadImg from './breakingBad.jpg'
+import deathNoteImg from './deathNote.jpg'
+import gameOfThronesImg from './gameofThrones.jpg'
+import WalkingDeadImg from './walkingDead.jpg'
+import WireImg from './theWire.jpg'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p className = "movieTagline">
-            This is the best and greatest movie in the world, this is just a tribute.
-          </p>
-          <img src={movieCover} className="movie-cover" alt="movie cover" />
+          <div className = "tvShows">
+          <h1 className="tv">Black Mirror</h1>
+          <h1 className="tv">Breaking Bad</h1>
+          <h1 className="tv">Death Note</h1>
+          <h1 className="tv">Game of Thrones</h1>
+          <h1 className="tv">The Walking Dead</h1>
+          <h1 className="tv">The Wire</h1>
+          </div>
         </header>
         <body>
 
